1fef86cbf36391ca6b29c6d1fb13904b9be9232a
Yiyi Sun <yiyisun@gmail.com>
2020-02-22 10:08:28 -0500

    component should use this.run instead of app.run

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -12,20 +12,20 @@ type State = {
 
 export default class Todo extends Component {
 
-const state: State = {
+ state: State = {
   filter: 'All',
   list: []
 };
 
-const view = (state: State) => {
+ view = (state: State) => {
   const countAll = state.list.length;
   const countActive = (state.list.filter(todo => !todo.done) as any).length || 0;
   const countComplete = state.list.filter(todo => todo.done).length || 0;
 
   return <div>
-    <button onclick={() => app.run("history-prev")}> Back </button>
-    <button onclick={() => app.run("history-next")}> Forward </button>
-    <p><input onkeyup={e => app.run('keyup', e)} /></p>
+    <button onclick={() => this.run("history-prev")}> Back </button>
+    <button onclick={() => this.run("history-next")}> Forward </button>
+    <p><input onkeyup={e => this.run('keyup', e)} /></p>
     <ul> {
       state.list
       .map((todo, idx) => ({ ...todo, idx }))
@@ -33,35 +33,35 @@ const view = (state: State) => {
       (state.filter === 'Active' && !todo.done) ||
       (state.filter === 'Complete' && todo.done))
       .map((todo) => <li>
-      <input type="checkbox" onclick={() => app.run('toggle-item', todo.idx)} checked={todo.done} />
-      <span>{todo.title} {' '} (<a href='#' onclick={() => app.run('delete-item', todo.idx)}>␡</a>)</span>
+      <input type="checkbox" onclick={() => this.run('toggle-item', todo.idx)} checked={todo.done} />
+      <span>{todo.title} {' '} (<a href='#' onclick={() => this.run('delete-item', todo.idx)}>␡</a>)</span>
       </li>)
     }</ul>
       <div>
-      <a href='#' onclick={e => app.run('filter-item', e)}>All</a> {` (${countAll}) | `}
-      <a href='#' onclick={e => app.run('filter-item', e)}>Active</a> {`(${countActive}) | `}
-      <a href='#' onclick={e => app.run('filter-item', e)}>Complete</a> {`(${countComplete})`}
+      <a href='#' onclick={e => this.run('filter-item', e)}>All</a> {` (${countAll}) | `}
+      <a href='#' onclick={e => this.run('filter-item', e)}>Active</a> {`(${countActive}) | `}
+      <a href='#' onclick={e => this.run('filter-item', e)}>Complete</a> {`(${countComplete})`}
       </div>
       <a href='#home'>Home</a> | <a href='#about'>About</a>
     </div>
 
 };
 
-const update = {
+ update = {
   '#todo': state => state,
-  'new-state': (_, state) => state,
-  'add-item': (state, title) => { app.run('save-state', {
+  '@new-state': (_, state) => state,
+  'add-item': (state, title) => { this.run('@save-state', {
     ...state,
     list: [...state.list, { title, done: false }]
   })},
-  'delete-item': (state, idx) => { app.run('save-state', {
+  'delete-item': (state, idx) => { this.run('@save-state', {
     ...state,
     list: [
       ...state.list.slice(0, idx),
       ...state.list.slice(idx + 1)
     ]
   })},
-  'toggle-item': (state, idx) => { app.run('save-state', {
+  'toggle-item': (state, idx) => { this.run('@save-state', {
     ...state,
     list: [
       ...state.list.slice(0, idx),
@@ -69,11 +69,11 @@ const update = {
       ...state.list.slice(idx + 1)
     ]
   })},
-  'filter-item': (state, e) => { app.run('save-state', {
+  'filter-item': (state, e) => { this.run('@save-state', {
     ...state, filter: e.target.textContent })},
   'keyup': (state, e) => {
     if (e.keyCode === 13 && e.target.value.trim()) {
-      app.run('add-item', e.target.value);
+      this.run('add-item', e.target.value);
       e.target.value = '';
     }
   },
